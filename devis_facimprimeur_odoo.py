3cf7d6b8996fe14c251094b216756b77239abd49
Fred-Perso <127020205+Fred-Perso@users.noreply.github.com>
2023-03-16 05:52:03 +0100

    Add files via upload

---
+++ devis_facimprimeur_odoo.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+# Fonction de calcul de prix
+def calcul_prix(produit, quantite):
+	
+	A4 = [0.4, 0.3, 0.2, 0.15, 0.12]
+	_135gr = [0.2, 0.2, 0.1, 0.1, 0.1]
+	_170gr = [3, 4, 5, 6, 7]
+	recto = [3, 4, 5, 6, 7]
+	quantites = [1,10, 20, 30, 40]
+	
+	# attribut_1, attribut_2, attribut_3 = produit
+	
+	prix_unitaire = [0, 0, 0]
+	
+#Tableau des coefs
+	coef=[0.5,1,2]
+# on détermine le chef en fonction du format
+	coefP=0
+	if "A4" in str(produit):
+		coefP = coef[1]
+	elif "A3" in str(produit):
+		coefP = coef[2]
+	elif "A5" in str(produit):
+		coefP = coef[0]
+		
+	Qt1 = 0
+	if quantite <= 1:
+		Qt1 = 0
+	elif quantite <= 10:
+		Qt1 = 1
+	elif quantite <= 20:
+		Qt1 = 2
+	elif quantite <= 30:
+		Qt1 = 3
+	else:
+		Qt1 = 4	
+		
+	if "A4" in str(produit):
+		prix_unitaire[0] = A4[Qt1]*coefP
+	elif "A5" in str(produit):
+		prix_unitaire[0] = A4[Qt1]*coefP
+	elif "A3" in str(produit):
+		prix_unitaire[0] = A4[Qt1]*coefP
+	else:
+		prix_unitaire[0] = 0
+		
+	if "135gr" in str(produit):
+		prix_unitaire[1] = _135gr[Qt1]*coefP
+	elif "150gr" in str(produit):
+		prix_unitaire[1] = _170gr[Qt1]*coefP
+	else:
+		prix_unitaire[1] = 0
+		
+	if "recto" in str(produit):
+		prix_unitaire[2] = recto[Qt1]*coefP
+	else:
+		prix_unitaire[0] = 0
+		
+	#somme de tout les prix unitaitaires x QT(Odoo)
+	prix_total = sum(prix_unitaire)
+	
+	return prix_total
+
+for record in self:
+	produit = record['name']
+	quantite = int(record['product_uom_qty'])
+	prix = calcul_prix(produit, quantite)
+	record['x_studio_test'] = prix
